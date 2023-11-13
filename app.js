37bb52831014ea427e673b4786aa657a520ec8e1
given02 <votmdnjem_id@naver.com>
2023-11-13 12:43:52 +0900

    init: express, mongoose, nodemon, dotenv 설치

---
+++ app.js
@@ -0,0 +1,48 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const DBConnection = require('./src/db/db-connection');
+const indexRouter = require('./src/routers/index');
+const usersRouter = require('./src/routers/users');
+require('dotenv').config();
+
+const app = express();
+
+// MongoDB 연결
+DBConnection();
+
+// 애플리케이션 설정
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CORS 설정
+app.use(cors());
+
+// 라우팅 설정
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
