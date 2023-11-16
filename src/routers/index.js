3873d8196f98078c4375db8bf71680a8da3ffe1a
[SW6] 유수빈 <syubim@gmail.com>
2023-11-16 19:39:06 +0900

    🎯 Fix 인덱스 파일에 스킬내용 추가

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,16 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const postRouter = require('./post-router');
-const commentRouter = require('./comment-router');
-const userRouter = require('./user-router');
+const postRouter = require("./post-router");
+const commentRouter = require("./comment-router");
+const userRouter = require("./user-router");
+const skillRouter = require("./skill-router");
+const userskillRouter = require("./user-skill-router");
 
-router.use('/api/v1/posts', postRouter);
-router.use('/api/v1/comments', commentRouter);
-router.use('/api/v1/users', userRouter);
+router.use("/api/v1/posts", postRouter);
+router.use("/api/v1/comments", commentRouter);
+router.use("/api/v1/users", userRouter);
+router.use("/", skillRouter);
+router.use("/", userskillRouter);
 
 /* GET home page. */
-router.get('/api', function(req, res, next) {
-  res.send('hello world!');
+router.get("/api", function (req, res, next) {
+  res.send("hello world!");
 });
 
 module.exports = router;
