dfea07d4faf6c969a7e9ee96eaf3012676ee0c8d
kyung <bkykim12@gmail.com>
2023-11-20 17:06:46 +0900

    ✨ Feat: 페이지네이션 구현

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -8,6 +8,35 @@ const postType = {
     review: "review"
 };
 
+const paginatePosts = async (list, page, pageSize) => {
+    if (!page && !pageSize) {
+        return {
+            paginatedPosts: list,
+            pageInfo: null,
+        };
+    }
+    
+    // 페이징 기본 설정 : 1페이지 당 5 게시물
+    page = parseInt(page) || 1;
+    pageSize = parseInt(pageSize) || 5;
+
+    const totalPosts = list.length;
+    const totalPages = Math.ceil(totalPosts / pageSize);
+    const skip = (page - 1) * pageSize;
+
+    const paginatedPosts = list.slice(skip, skip + pageSize);
+
+    return {
+        paginatedPosts,
+        pageInfo: {
+            page: parseInt(page),
+            pageSize: parseInt(pageSize),
+            totalPosts,
+            totalPages,
+        },
+    };
+};
+
 const addPost = async (req, res, next) => {
     const { title, content, category, author } = req.body;
 
@@ -27,26 +56,39 @@ const addPost = async (req, res, next) => {
 const findPostByCategory = async (req, res, next) => {
     const { category } = req.params;
 
-    if(!postType[category]){
+    if (!postType[category]) {
         throw {
             status: 400,
-            message: "게시글 카테고리를 확인해주세요"
+            message: "게시글 카테고리를 확인해주세요",
         };
     }
 
-    const list = await postService.getPostByCategory(category);
+    const list = await postService.findPostByCategory(category);
+    let { page, pageSize } = req.query;
+
+    const { paginatedPosts, pageInfo } = await paginatePosts(list, page, pageSize);
+
     res.status(200).json({
-        message: `${category} 카테고리 요청 결과`,
-        data: list
-    })
+        message: `${category} 카테고리 ${page ? `${pageInfo.page} 페이지` : '모든'} 게시글 목록`,
+        data: {
+            posts: paginatedPosts,
+            pageInfo,
+        },
+    });
 };
 
 const findAllPost = async (req, res, next) => {
-    const list = await postService.getAllPost();
+    let { page, pageSize } = req.query;
+    const allPosts = await postService.findAllPost();
+
+    const { paginatedPosts, pageInfo } = await paginatePosts(allPosts, page, pageSize);
 
     res.status(200).json({
-        message: "전체 리스트",
-        data: list
+        message: `${page ? `${pageInfo.page} 페이지` : '모든'} 게시글 조회`,
+        data: {
+            posts: paginatedPosts,
+            pageInfo,
+        },
     });
 };
 
