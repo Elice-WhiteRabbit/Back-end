badfd1a7a535242771c5beaf6115ee314f3e9140
[SW6] 유수빈 <syubim@gmail.com>
2023-11-22 11:35:18 +0900

    fix 머지충돌 수정

--- src/controllers/link-controller.js
+++ src/controllers/link-controller.js
@@ -8,7 +8,7 @@ exports.updateLinks = async (req, res) => {
   try {
     const userId = req.params.id; // 라우트에서 :id를 사용하므로 req.params.id를 사용해야 합니다.
     const links = req.body.links;
-    const updatedUser = await updateUserLinks(userId, links); // 여기서 함수 이름을 올바르게 사용합니다.
+    const updatedUser = await updateUserLinks(userId, links);
 
     if (!updatedUser) {
       return res.status(404).json({ message: 'User not found' });
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,33 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const { imageToURL } = require('../utils/image-to-url');
 const postRouter = require('./post-router');
 const commentRouter = require('./comment-router');
 const userRouter = require('./user-router');
+const skillRouter = require('./skill-router');
 const mailer = require('../utils/mailer');
 
 router.use('/api/v1/posts', postRouter);
 router.use('/api/v1/comments', commentRouter);
 router.use('/api/v1/users', userRouter);
 router.post('/api/v1/image', imageToURL);
-=======
-const { imageToURL } = require("../utils/image-to-url");
-const postRouter = require("./post-router");
-const commentRouter = require("./comment-router");
-const userRouter = require("./user-router");
-const skillRouter = require("./skill-router");
-
-router.use("/api/v1/posts", postRouter);
-router.use("/api/v1/comments", commentRouter);
-router.use("/api/v1/users", userRouter);
-router.use("/api/v1/skills", skillRouter);
-router.use("./api/v1/image", imageToURL);
->>>>>>> e8e9349c5f6946919ac1e9ce3ffbe92127d2b96f
+router.use('/api/v1/skills', skillRouter);
 
 /* GET home page. */
-router.get("/api", function (req, res, next) {
-  res.send("hello world!");
+router.get('/api', function (req, res, next) {
+  res.send('hello world!');
 });
 
 module.exports = router;
