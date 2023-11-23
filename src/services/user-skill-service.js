1a1a4ddc3e76920c7bba268a30efb8aa4d4ae570
[SW6] 유수빈 <syubim@gmail.com>
2023-11-23 15:51:53 +0900

    ✨ Feat: 스킬 populate 추가해서 이름 뜨게 함

--- src/services/user-skill-service.js
+++ src/services/user-skill-service.js
@@ -1,4 +1,4 @@
-const { User } = require("../db");
+const { User } = require('../db');
 
 //get 스킬에 해당되는 문서만 가져오기 (ex:스킬a를 가지고있는 모든 유저 불러오기)
 
@@ -13,20 +13,18 @@ const getUsersBySkill = async (skill) => {
 //관계 문서 추가 (ex:유저a - 스킬b를 추가할 때)
 const addUserSkill = async (data) => {
   const user = await User.findById(data.userid);
-  const skill = String(user).includes(data.skill);
+  const skillExists = user.skills.includes(data.skill);
 
-  if (skill == true) {
+  if (skillExists) {
     throw {
       status: 409,
-      message: "이미 존재하는 스킬입니다",
+      message: '이미 존재하는 스킬입니다',
     };
   }
 
-  return await User.findByIdAndUpdate(
-    data.userid,
-    { $push: { skills: data.skill } }
-    // { new: false }
-  );
+  await User.findByIdAndUpdate(data.userid, { $push: { skills: data.skill } });
+
+  return await User.findById(data.userid).populate('skills', 'skill');
 };
 
 //관계 문서 수정은 필요 없을 것 같음. (추가 or 삭제 뿐이니까..)
