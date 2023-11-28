467e465867d90bbd721efddc6707435b1dbec7eb
kyung <bkykim12@gmail.com>
2023-11-29 00:12:38 +0900

    ✨ Feat: 게시물, 댓글 joi 적용

--- src/routers/comment-router.js
+++ src/routers/comment-router.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const commentController = require('../controllers/comment-controller');
 const asyncHandler = require('../utils/async-handler');
 const { auth } = require('../middlewares/verify-token');
+const { commentValidation } = require('../middlewares/validation');
 
-router.post('/', auth, asyncHandler(commentController.addComment));
+router.post('/', auth, commentValidation.addComment, asyncHandler(commentController.addComment));
 router.get('/boards/:boardId', asyncHandler(commentController.findCommentsByPost));
 router.get('/users/:userId/comments', asyncHandler(commentController.findCommentsByUser));
 router.get('/comments/:commentId', commentController.findCommentById);
-router.patch('/:id', auth, asyncHandler(commentController.modifyComment));
+router.patch('/:id', auth, commentValidation.modifyComment, asyncHandler(commentController.modifyComment));
 router.delete('/:id', auth, asyncHandler(commentController.removeComment));
 
 module.exports = router;
